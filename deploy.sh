0569b92c5d335d60df44336e3337f6bd144d73a3
wangzedong <31271577+zedwanger@users.noreply.github.com>
2021-05-24 15:25:42 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-# npm run build
+npm run build
 
 # 进入生成的文件夹（ 进入VuePress打包后的文件夹根目录 ）
-# cd .vuepress/dist
+cd public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f git@github.com:zedwanger.github.io/myblogs.git master
+git push -f git@github.com:zedwanger.github.io/myblog.git master:gh-pages
 
 # cd -
