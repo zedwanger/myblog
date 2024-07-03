89755bf29d7be89cc733fb4c7775d3cbceece526
zedwanger <31271577+zedwanger@users.noreply.github.com>
2024-07-03 08:54:34 +0000

    Deploying to gh-pages from @ zedwanger/myblog@ea484ea74b15225d202cb3bf54b1c3e15718173f 🚀

--- .vuepress/config/theme/index.js
+++
@@ -1,9 +0,0 @@
-const themeReco = require('./themeReco.js')
-
-const sidebar = require('../sidebar/')
-const nav = require('../nav')
-
-module.exports = Object.assign({}, themeReco, {
-  sidebar,
-  nav
-})
